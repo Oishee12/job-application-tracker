b6721936c464b9c45a7b43e982a24193c4396aa4
Oishe <yhsiokhan@gmail.com>
2026-03-01 20:27:56 +0600

    add functionality to three buttons; work in progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 let currentTab = 'all';
+
 const tabInactive = ["bg-white"];
 const tabActive = ["bg-primary", "text-white"];
+
+const allJob = document.getElementById('all-job');
+const allInterview = document.getElementById('all-interview');
+const allReject = document.getElementById('all-reject');
+
 function switchedTab(tab){
     const tabs = ["all", "interview", "rejected"];
     for(const t of tabs){
@@ -12,6 +18,21 @@ function switchedTab(tab){
        else{
         tabName.classList.remove(...tabActive);
         tabName.classList.add(...tabInactive);
+       }
+
+       const pages = [allJob, allInterview, allReject];
+       for(const section of pages){
+        section.classList.add('hidden');
+       }
+
+       if(tab === "all"){
+        allJob.classList.remove('hidden');
+       }
+       else if(tab === 'interview'){
+        allInterview.classList.remove('hidden');
+       }
+       else{
+        allReject.classList.remove('hidden');
        }
     }
 }
